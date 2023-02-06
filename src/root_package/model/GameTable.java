e7e1526e24d1ded1c69a884b075eea11e430a769
bodkiny <bogdan2804@gmail.com>
2023-02-07 01:34:33 +0200

    Add model classes

--- src/root_package/model/GameTable.java
+++ src/root_package/model/GameTable.java
@@ -1,4 +1,22 @@
 package root_package.model;
 
 public class GameTable {
+
+    private final char[][] table = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+    };
+
+    public boolean isEmpty(final Cell cell) {
+        return table[cell.getRow()][cell.getCol()] == ' ';
+    }
+
+    public char getSigh(final Cell cell) {
+        return table[cell.getRow()][cell.getCol()];
+    }
+
+    public void setSign(final Cell cell, final char sigh) {
+        table[cell.getRow()][cell.getCol()] = sigh;
+    }
 }
