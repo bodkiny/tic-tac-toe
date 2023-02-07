59e6fd92e5d4a4d8ebb0c6d70e0bd16f3bc87b34
bodkiny <bogdan2804@gmail.com>
2023-02-07 02:09:37 +0200

    Implement DataDisplayer class methods

--- src/root_package/logic/DataDisplayer.java
+++ src/root_package/logic/DataDisplayer.java
@@ -1,13 +1,27 @@
 package root_package.logic;
 
+import root_package.model.Cell;
 import root_package.model.GameTable;
 
 public class DataDisplayer {
     public void displayMappingTable() {
-
+        System.out.println("-------------\n" +
+                "| 7 | 8 | 9 |\n" +
+                "-------------\n" +
+                "| 4 | 5 | 6 |\n" +
+                "-------------\n" +
+                "| 1 | 2 | 3 |\n" +
+                "-------------");
     }
 
     public void displayTable(GameTable gameTable) {
-
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + gameTable.getSigh(new Cell(i, j)) + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("-------------");
     }
 }
