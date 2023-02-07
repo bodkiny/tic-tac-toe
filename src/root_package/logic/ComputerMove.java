a9f1a2ea7172dfe065eed9d61f38b821c6e21a07
bodkiny <bogdan2804@gmail.com>
2023-02-07 02:39:45 +0200

    Implement random computer move

--- src/root_package/logic/ComputerMove.java
+++ src/root_package/logic/ComputerMove.java
@@ -1,9 +1,23 @@
 package root_package.logic;
 
+import root_package.model.Cell;
 import root_package.model.GameTable;
+
+import java.util.Random;
 
 public class ComputerMove {
     public void make(GameTable gameTable) {
 
+        while (true) {
+            final Random random = new Random();
+            final int row = random.nextInt(3);
+            final int col = random.nextInt(3);
+            Cell randomCell = new Cell(row, col);
+
+            if (gameTable.isEmpty(randomCell)) {
+                gameTable.setSign(randomCell, 'O');
+                return;
+            }
+        }
     }
 }
