e1aa485633da447f1da1aed5f8abfbd2c8a5b870
bodkiny <bogdan2804@gmail.com>
2023-02-08 22:36:14 +0200

    Implement UserMove class

--- src/root_package/logic/UserMove.java
+++ src/root_package/logic/UserMove.java
@@ -1,9 +1,41 @@
 package root_package.logic;
 
+import root_package.model.Cell;
 import root_package.model.GameTable;
+
+import java.util.Scanner;
 
 public class UserMove {
     public void make(GameTable gameTable) {
+        while (true) {
+            final Cell cell = getUserInput();
+            if (gameTable.isEmpty(cell)) {
+                gameTable.setSign(cell, 'X');
+                return;
+            } else {
+                System.out.println("Can't make a move, because the cell is not empty! Try again!");
+            }
+        }
+    }
 
+    private Cell getUserInput() {
+        while (true) {
+            System.out.print("Choose the empty cell for your move (enter a number between 1 & 9): ");
+            final String userInput = new Scanner(System.in).nextLine();
+            if (userInput.length() == 1) {
+                final char ch = userInput.charAt(0);
+                if (ch >= '1' && ch <= '9') {
+                    int count = 9;
+                    for (int i = 0; i < 3; i++) {
+                        for (int j = 2; j >= 0; j--) {
+                            if (count == Integer.parseInt(userInput)) {
+                                return new Cell(i, j);
+                            }
+                            count--;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
